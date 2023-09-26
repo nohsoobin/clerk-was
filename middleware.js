d581fdea16dd8d5f8c42b2e6d893f7de7b3efbf0
nohsoobin <nohsoobin18@naver.com>
2023-09-26 11:23:27 +0900

    0926

---
+++ middleware.js
@@ -0,0 +1,8 @@
+import { authMiddleware } from '@clerk/nextjs'
+
+export default authMiddleware({
+  publicRoutes: ['/', '/api/courses', '/api/repos'],
+})
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+}
